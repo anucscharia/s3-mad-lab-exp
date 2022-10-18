b7fbc4f60fba7dc396d759120fbfcf1ef44f9ad9
anucscharia <anubinuscharia@gmail.com>
2022-10-18 12:12:29 +0530

    updation

--- 4_Calculator/app/src/main/java/com/example/a4_calculator/MainActivity.java
+++ 4_Calculator/app/src/main/java/com/example/a4_calculator/MainActivity.java
@@ -9,224 +9,275 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
-    TextView e1,t1;
+    TextView res,text;
     Double sum;
-    String op;
+    String op,r,t,last,str;
+    int s,i,status_add,status_div,status_mul,status_sub,status_dot=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e1=findViewById(R.id.e1);
-        t1=findViewById(R.id.t1);
+        res=findViewById(R.id.res);
+        text=findViewById(R.id.text);
         findViewById(R.id.btn_1).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "1");
-
+                addvalue("1");
             }
         });
         findViewById(R.id.btn_2).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "2");
+                addvalue("2");
             }
         });
         findViewById(R.id.btn_3).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "3");
+                addvalue("3");
             }
         });
         findViewById(R.id.btn_4).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "4");
+                addvalue("4");
             }
         });
         findViewById(R.id.btn_5).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "5");
+                addvalue("5");
             }
         });
         findViewById(R.id.btn_6).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "6");
+                addvalue("6");
             }
         });
         findViewById(R.id.btn_7).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "7");
+                addvalue("7");
             }
         });
         findViewById(R.id.btn_8).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "8");
+                addvalue("8");
             }
         });
         findViewById(R.id.btn_9).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "9");
+                addvalue("9");
             }
         });
         findViewById(R.id.btn_0).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "0");
+                addvalue("10");
             }
         });
         findViewById(R.id.dot).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + ".");
+                if(status_dot!=1) {
+                    addvalue( ".");
+                    status_dot=1;
+                }
             }
         });
         findViewById(R.id.button_open_bracket).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + "(");
+                addvalue("(");
             }
         });
         findViewById(R.id.button_close_bracket).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText(e1.getText().toString() + ")");
+                addvalue(")");
             }
         });
         findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                t1.setText(t1.getText().toString() + e1.getText().toString());
-                e1.setText("");
-                op="+";
-                t1.setText(t1.getText().toString() + op);
+                if(0==status_add) {
+                    last = text.getText().toString();
+                    op = "+";
+                    status_add=1;
+                    if ((last.endsWith("-")) || (last.endsWith("*")) || (last.endsWith("/"))) {
+                        text.setText(last.substring(0, last.length() - 1));
+                        status_div=status_mul=status_sub=0;
+                    }
+                    text.setText(text.getText().toString() + op);
+                }
+                status_dot=0;
             }
         });
         findViewById(R.id.sub).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                t1.setText(t1.getText().toString() + e1.getText().toString());
-                e1.setText("");
-                op = "-";
-                t1.setText(t1.getText().toString() + op);
+                if(0==status_sub) {
+                    last = text.getText().toString();
+                    op = "-";
+                    status_sub=1;
+                    if ((last.endsWith("+")) || (last.endsWith("*")) || (last.endsWith("/"))) {
+                        text.setText(last.substring(0, last.length() - 1));
+                        status_add=status_div=status_mul=0;
+                    }
+                    text.setText(text.getText().toString() + op);
+                }
+                status_dot=0;
             }
         });
         findViewById(R.id.mul).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                t1.setText(t1.getText().toString() + e1.getText().toString());
-                e1.setText("");
-                op = "*";
-                t1.setText(t1.getText().toString() + op);
+                if(0==status_mul) {
+                    last = text.getText().toString();
+                    op = "*";
+                    status_mul=1;
+                    if ((last.endsWith("-")) || (last.endsWith("+")) || (last.endsWith("/"))) {
+                        text.setText(last.substring(0, last.length() - 1));
+                        status_add=status_div=status_sub=0;
+                    }
+                    text.setText(text.getText().toString() + op);
+                }
+                status_dot=0;
             }
         });
         findViewById(R.id.div).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                t1.setText(t1.getText().toString() + e1.getText().toString());
-                e1.setText("");
-                op = "/";
-                t1.setText(t1.getText().toString() + op);
+                if(0==status_div) {
+                    last = text.getText().toString();
+                    op = "/";
+                    status_div=1;
+                    if ((last.endsWith("-")) || (last.endsWith("*")) || (last.endsWith("+"))) {
+                        text.setText(last.substring(0, last.length() - 1));
+                        status_add=status_mul=status_sub=0;
+                    }
+                    text.setText(text.getText().toString() + op);
+                }
+                status_dot=0;
             }
         });
         findViewById(R.id.eq).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                t1.setText(t1.getText().toString()+e1.getText().toString());
-                String str = t1.getText().toString();
-                int s=evaluate(str);
-                t1.setText(""+s);e1.setText("");
+                str=text.getText().toString();
+                if ((str.endsWith("+")) ||(str.endsWith("-")) || (str.endsWith("*")) || (str.endsWith("/"))) {
+                    text.setText(str.substring(0, str.length() - 1));
+                    s=calculate();
+                    res.setText(""+s);
+                }
+                else {
+                    s = calculate();
+                    text.setText("" + s);
+                    res.setText("");
+                }
+                status_dot = 0;
             }
         });
         findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                String text=e1.getText().toString();
-                e1.setText(text.substring(0, text.length() - 1));
+                str=text.getText().toString();
+                text.setText(str.substring(0, str.length() - 1));
+                str=text.getText().toString();
+                s=calculate();
+                res.setText(""+s);
             }
         });
         findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                e1.setText("");
-                t1.setText("");
+                text.setText("");
+                res.setText("");
             }
         });
     }
-        public int evaluate (String exp)
-        {
-            Stack<Integer> operands = new Stack<>();  //Operand stack
-            Stack<Character> operations = new Stack<>();  //Operator stack
-            for (int i = 0; i < exp.length(); i++) {
-                char c = exp.charAt(i);
-                if (Character.isDigit(c))   //check if it is number
-                {
-                    //Entry is Digit, and it could be greater than a one-digit number
-                    int num = 0;
-                    while (Character.isDigit(c)) {
-                        num = num * 10 + (c - '0');
-                        i++;
-                        if (i < exp.length()) {
-                            c = exp.charAt(i);
-                        } else {
-                            break;
-                        }
-                    }
-                    i--;
-                    operands.push(num);
-                } else if (c == '(') {
-                    operations.push(c);   //push character to operators stack
-                }
-                //Closed brace, evaluate the entire brace
-                else if (c == ')') {
-                    while (operations.peek() != '(') {
-                        int output = performOperation(operands, operations);
-                        operands.push(output);   //push result back to stack
-                    }
-                    operations.pop();
-                }
-
-                // current character is operator
-                else if (isOperator(c)) {
-                    while (!operations.isEmpty() && precedence(c) <= precedence(operations.peek())) {
-                        int output = performOperation(operands, operations);
-                        operands.push(output);   //push result back to stack
-                    }
-                    operations.push(c);   //push the current operator to stack
-                }
-            }
-
-            while (!operations.isEmpty()) {
-                int output = performOperation(operands, operations);
-                operands.push(output);   //push final result back to stack
-            }
-            return operands.pop();
-        }
-
-        static int precedence ( char c)
-        {
-            switch (c) {
-                case '+':
-                case '-':
-                    return 1;
-                case '*':
-                case '/':
-                    return 2;
-                case '^':
-                    return 3;
-            }
-            return -1;
-        }
-
-        public int performOperation (Stack < Integer > operands, Stack < Character > operations)
-        {
-            int a = operands.pop();
-            int b = operands.pop();
-            char operation = operations.pop();
-            switch (operation) {
-                case '+':
-                    return a + b;
-                case '-':
-                    return b - a;
-                case '*':
-                    return a * b;
-                case '/':
-                    if (a == 0) {
-                        System.out.println("Cannot divide by zero");
-                        return 0;
-                    }
-                    return b / a;
-            }
-            return 0;
-        }
-
-        public boolean isOperator ( char c)
-        {
-            return (c == '+' || c == '-' || c == '/' || c == '*' || c == '^');
-        }
-
+    public void addvalue(String str){
+        text.setText(text.getText().toString() +str);
+        status_add=status_div=status_mul=status_sub=0;
+        s=calculate();
+        res.setText(""+s);
     }
+    public int calculate(){
+        str=text.getText().toString();
+        s=evaluate(str);
+        status_sub=status_add=status_div=status_mul=0;
+        return s;
+    }
+    public int evaluate (String exp)
+    {
+        Stack<Integer> operands = new Stack<>();  //Operand stack
+        Stack<Character> operations = new Stack<>();  //Operator stack
+        for (i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            if (Character.isDigit(c))   //check if it is number
+            {
+                //Entry is Digit, and it could be greater than a one-digit number
+                int num = 0;
+                while (Character.isDigit(c)) {
+                    num = num * 10 + (c - '0');
+                    i++;
+                    if (i < exp.length()) {
+                        c = exp.charAt(i);
+                    } else {
+                        break;
+                    }
+                }
+                i--;
+                operands.push(num);
+            } else if (c == '(') {
+                operations.push(c);   //push character to operators stack
+            }
+            //Closed brace, evaluate the entire brace
+            else if (c == ')') {
+                while (operations.peek() != '(') {
+                    int output = performOperation(operands, operations);
+                    operands.push(output);   //push result back to stack
+                }
+                operations.pop();
+            }
+
+            // current character is operator
+            else if (isOperator(c)) {
+                while (!operations.isEmpty() && precedence(c) <= precedence(operations.peek())) {
+                    int output = performOperation(operands, operations);
+                    operands.push(output);   //push result back to stack
+                }
+                operations.push(c);   //push the current operator to stack
+            }
+        }
+
+        while (!operations.isEmpty()) {
+            int output = performOperation(operands, operations);
+            operands.push(output);   //push final result back to stack
+        }
+        return operands.pop();
+    }
+
+    static int precedence ( char c)
+    {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    public int performOperation (Stack < Integer > operands, Stack < Character > operations)
+    {
+        int a = operands.pop();
+        int b = operands.pop();
+        char operation = operations.pop();
+        switch (operation) {
+            case '+':
+                return a + b;
+            case '-':
+                return b - a;
+            case '*':
+                return a * b;
+            case '/':
+                if (a != 0)
+                    return b / a;
+        }
+        return 0;
+    }
+
+    public boolean isOperator ( char c)
+    {
+        return (c == '+' || c == '-' || c == '/' || c == '*' || c == '^');
+    }
+
+}
--- 5_shared_preference/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 5_shared_preference/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,46 +1,166 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView t1,t2;
-    EditText e1;
+
+    private TextView num1;
+    private TextView num2;
+    private TextView operator;
+    private EditText resultEntered;
+    private TextView level;
+    private TextView score;
+    private TextView status;
+    private Button nextLevel;
+    private  SharedPreferences sharedPreferences;
+
+    private void updateScore() {
+        int scr;
+        scr = Integer.parseInt(score.getText().toString());
+        scr = scr + 10;
+        score.setText("" + scr);
+    }
+    private void updateLevel() {
+        int lvl = Integer.parseInt(level.getText().toString());
+        lvl = lvl + 1;
+    }
+    private void resetUI() {
+        Random r = new Random();
+        int lvl = Integer.parseInt(String.valueOf((TextView) findViewById(R.id.levelvalue)));
+        int scr = Integer.parseInt(String.valueOf((TextView) findViewById(R.id.scorevalue)));
+
+        int r1 = r.nextInt(lvl * 10);
+        int r2 = r.nextInt(lvl * 10);
+        num1.setText("" + r1);
+        num2.setText("" + r2);
+        num1.setTextColor(Color.RED);
+        num2.setTextColor(Color.RED);
+        int op = r.nextInt( 3);
+        String opr = "+";
+        switch (op) {
+            case 3:
+                opr = "/";
+                if(r1<r2) {
+                    num1.setText("" + r2);
+                    num2.setText("" + r1);
+                }
+                break;
+            case 2:
+                opr = "*";
+                break;
+            case 1:
+                opr = "-";
+                if(r1<r2) {
+                    num1.setText("" + r2);
+                    num2.setText("" + r1);
+                }
+                break;
+        }
+        score.setText("" + scr);
+        level.setText("" + lvl);
+        operator.setText(opr);
+        resultEntered.setText("");
+        resultEntered.setTextColor(Color.BLACK);
+        nextLevel.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        t1=findViewById(R.id.textView);
-        t2=findViewById(R.id.textView2);
-        e1=findViewById(R.id.e1);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(TextUtils.isEmpty(e1.getText().toString())){
-                    Toast.makeText(getApplicationContext(),"Empty box",Toast.LENGTH_LONG).show();
-                    e1.setError("Enter Result");
-                }else {
-                    if((Integer.parseInt(t1.getText().toString())+Integer.parseInt(t2.getText().toString()))==Integer.parseInt(e1.getText().toString())){
-                        Toast.makeText(getApplicationContext(),"You WIN",Toast.LENGTH_LONG).show();
-                    }else {
-                        Toast.makeText(getApplicationContext(),"You LOSE",Toast.LENGTH_LONG).show();
-                    }
-                }
-                String n  = e1.getText().toString();
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-                editor.putString(e1,n);
-                editor.commit();
 
+        num1 = (TextView) findViewById(R.id.num1);
+        num2 = (TextView) findViewById(R.id.num2);
+        operator = (TextView)findViewById(R.id.operator);
+        resultEntered = (EditText) findViewById(R.id.result);
+        level = (TextView) findViewById(R.id.levelvalue) ;
+        score=(TextView) findViewById(R.id.scorevalue);
+        status=(TextView) findViewById(R.id.status);
+        nextLevel = (Button) findViewById(R.id.next);
+        nextLevel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                updateLevel();
+                resetUI();
             }
         });
-
+        resultEntered.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_DONE) {
+                    if(!TextUtils.isEmpty(resultEntered.getText().toString())) {
+                        int result = Integer.parseInt(resultEntered.getText().toString());
+                        int n1 = Integer.parseInt(num1.getText().toString());
+                        int n2 = Integer.parseInt(num2.getText().toString());
+                        String opr = operator.getText().toString();
+                        int resultActual = 0;
+                        switch (opr) {
+                            case "+":
+                                resultActual = n1 + n2;
+                                break;
+                            case "-":
+                                resultActual = n1 - n2;
+                                break;
+                            case "*":
+                                resultActual = n1 * n2;
+                                break;
+                            case "/":
+                                resultActual = n1 / n2;
+                                break;
+                        }
+                        if (result == resultActual) {
+                            status.setText("Correct");
+                            status.setTextColor(Color.GREEN);
+                            resultEntered.setTextColor(Color.GREEN);
+                            nextLevel.setVisibility(View.VISIBLE);
+                            updateScore();
+                        } else {
+                            status.setText("Wrong");
+                            status.setTextColor(Color.RED);
+                            resultEntered.setTextColor(Color.RED);
+                            nextLevel.setVisibility(View.INVISIBLE);
+                        }
+                        status.setVisibility(View.VISIBLE);
+                    }
+                }
+                return false;
+            }
+        });
+    }
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        int l =sh.getInt("level",0);
+        int s = sh.getInt("score", 0);
+        int n1 = sh.getInt("num1", 0);
+        int n2 = sh.getInt("num2", 0);
+        String o=sh.getString("op","");
+        level.setText(String.valueOf(l));
+        score.setText(String.valueOf(s));
+        num1.setText(String.valueOf(n1));
+        num2.setText(String.valueOf(n2));
+        operator.setText(o);
     }
 
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+        myEdit.putInt("level", Integer.parseInt(level.getText().toString()));
+        myEdit.putInt("score", Integer.parseInt(score.getText().toString()));
+        myEdit.putInt("num1", Integer.parseInt(num1.getText().toString()));
+        myEdit.putInt("num2", Integer.parseInt(num2.getText().toString()));
+        myEdit.putString("op",operator.getText().toString());
+        myEdit.apply();
+    }
 }
