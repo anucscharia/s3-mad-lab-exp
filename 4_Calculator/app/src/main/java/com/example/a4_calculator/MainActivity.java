a1824b8503fa38e8d2a24f3fcbd13dcfe44a2b39
anucscharia <anubinuscharia@gmail.com>
2022-11-01 11:59:20 +0530

    calculator updation

--- 4_Calculator/app/src/main/java/com/example/a4_calculator/MainActivity.java
+++ 4_Calculator/app/src/main/java/com/example/a4_calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a4_calculator;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,9 +11,9 @@ import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
     TextView res,text;
-    Double sum;
+    Double s;
     String op,r,t,last,str;
-    int s,i,status_add,status_div,status_mul,status_sub,status_dot=0;
+    int i,operator,status_dot=0,open,close;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,23 +80,30 @@ public class MainActivity extends AppCompatActivity {
         });
         findViewById(R.id.button_open_bracket).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                addvalue("(");
+                text.setText(text.getText().toString() +"(");
+                operator=status_dot=0;
+                open=1;
+                close=0;
             }
         });
         findViewById(R.id.button_close_bracket).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                addvalue(")");
+                if(open==1) {
+                    text.setText(text.getText().toString() + ")");
+                    operator= status_dot = 0;
+                    close=1;
+                    open=0;
+                }
             }
         });
         findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(0==status_add) {
+                if(0==operator) {
                     last = text.getText().toString();
                     op = "+";
-                    status_add=1;
+                    operator=1;
                     if ((last.endsWith("-")) || (last.endsWith("*")) || (last.endsWith("/"))) {
                         text.setText(last.substring(0, last.length() - 1));
-                        status_div=status_mul=status_sub=0;
                     }
                     text.setText(text.getText().toString() + op);
                 }
@@ -104,13 +112,12 @@ public class MainActivity extends AppCompatActivity {
         });
         findViewById(R.id.sub).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(0==status_sub) {
+                if(0==operator) {
                     last = text.getText().toString();
                     op = "-";
-                    status_sub=1;
+                    operator=1;
                     if ((last.endsWith("+")) || (last.endsWith("*")) || (last.endsWith("/"))) {
                         text.setText(last.substring(0, last.length() - 1));
-                        status_add=status_div=status_mul=0;
                     }
                     text.setText(text.getText().toString() + op);
                 }
@@ -119,13 +126,12 @@ public class MainActivity extends AppCompatActivity {
         });
         findViewById(R.id.mul).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(0==status_mul) {
+                if(0==operator) {
                     last = text.getText().toString();
                     op = "*";
-                    status_mul=1;
+                    operator=1;
                     if ((last.endsWith("-")) || (last.endsWith("+")) || (last.endsWith("/"))) {
                         text.setText(last.substring(0, last.length() - 1));
-                        status_add=status_div=status_sub=0;
                     }
                     text.setText(text.getText().toString() + op);
                 }
@@ -134,13 +140,12 @@ public class MainActivity extends AppCompatActivity {
         });
         findViewById(R.id.div).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(0==status_div) {
+                if(0==operator) {
                     last = text.getText().toString();
                     op = "/";
-                    status_div=1;
+                    operator=1;
                     if ((last.endsWith("-")) || (last.endsWith("*")) || (last.endsWith("+"))) {
                         text.setText(last.substring(0, last.length() - 1));
-                        status_add=status_mul=status_sub=0;
                     }
                     text.setText(text.getText().toString() + op);
                 }
@@ -151,133 +156,170 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 str=text.getText().toString();
                 if ((str.endsWith("+")) ||(str.endsWith("-")) || (str.endsWith("*")) || (str.endsWith("/"))) {
-                    text.setText(str.substring(0, str.length() - 1));
-                    s=calculate();
-                    res.setText(""+s);
+                    str=str.substring(0, str.length() - 1);
+                    if(str.endsWith("(")){
+                        str=str.substring(0, str.length() - 1);
+                        open=0;
+                    }
+                    if(open==1&&close==0) {
+                        str=str+")";
+                    }
+                    s = calculate(str);
+                    text.setText(""+s);
+                }
+                else if(str.endsWith("(")){
+                    str=str.substring(0, str.length() - 1);
+                    open=0;
+                    if ((str.endsWith("+")) ||(str.endsWith("-")) || (str.endsWith("*")) || (str.endsWith("/"))) {
+                        str=str.substring(0, str.length() - 1);
+                    }
+                    s = calculate(str);
+                    text.setText(""+s);
                 }
                 else {
-                    s = calculate();
-                    text.setText("" + s);
-                    res.setText("");
+                    if(open==1&&close==0)
+                        str=str+")";
+                    s = calculate(str);
+                    text.setText(""+s);
                 }
-                status_dot = 0;
+                res.setText("");
+                status_dot =open=close= 0;
             }
         });
         findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 str=text.getText().toString();
                 text.setText(str.substring(0, str.length() - 1));
+                String l=str.substring(str.length() -1);
+                if((l=="+")||(l=="-")||(l=="*")||(l=="/"))
+                    operator=0;
+                else if(l==".")
+                    status_dot=0;
+                else if(l==")")
+                    close=0;
+                else if(l=="(")
+                    open=0;
                 str=text.getText().toString();
-                s=calculate();
-                res.setText(""+s);
+                if (!(str.endsWith("+")) &&!(str.endsWith("-")) && !(str.endsWith("*")) && !(str.endsWith("/"))&&!(str.endsWith("("))&&!(open==1&&close==0)) {
+                    s = calculate(str);
+                    res.setText(""+s);
+                }
+                else  if ((str.endsWith("+")) ||(str.endsWith("-")) || (str.endsWith("*")) || (str.endsWith("/"))) {
+                    res.setText("");
+                }
+                else if(str.endsWith("(")){
+                    res.setText("");
+                }
+                else if(open==1&&close==0) {
+                    res.setText("");
+                }
             }
         });
         findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 text.setText("");
                 res.setText("");
+                operator=status_dot=open=close=0;
             }
         });
     }
     public void addvalue(String str){
         text.setText(text.getText().toString() +str);
-        status_add=status_div=status_mul=status_sub=0;
-        s=calculate();
+        operator=0;
+        str=text.getText().toString();
+        if ((str.endsWith("+")) ||(str.endsWith("-")) || (str.endsWith("*")) || (str.endsWith("/")))
+            str=str.substring(0, str.length() - 1);
+        if((open==1)&&(close==0))
+            str=str+")";
+        s=calculate(str);
         res.setText(""+s);
     }
-    public int calculate(){
-        str=text.getText().toString();
+    public Double calculate(String str){
+
         s=evaluate(str);
-        status_sub=status_add=status_div=status_mul=0;
+        operator=0;
         return s;
     }
-    public int evaluate (String exp)
-    {
-        Stack<Integer> operands = new Stack<>();  //Operand stack
-        Stack<Character> operations = new Stack<>();  //Operator stack
-        for (i = 0; i < exp.length(); i++) {
-            char c = exp.charAt(i);
-            if (Character.isDigit(c))   //check if it is number
-            {
-                //Entry is Digit, and it could be greater than a one-digit number
-                int num = 0;
-                while (Character.isDigit(c)) {
-                    num = num * 10 + (c - '0');
-                    i++;
-                    if (i < exp.length()) {
-                        c = exp.charAt(i);
-                    } else {
-                        break;
-                    }
+    public Double evaluate(String expression) {
+        System.out.println(expression);
+        //stack for operands
+        Stack<Double> numberStack = new Stack<>();
+
+        //stack for characters
+        Stack<Character> operatorStack = new Stack<>();
+
+        char []expToken = expression.toCharArray();
+        for(int i=0; i<expToken.length; i++) {
+
+            char ch = expToken[i];
+
+            //Handling space in the expression
+            if(ch == ' ') {
+                continue;
+            }
+            else if(Character.isDigit(ch) || ch == '.') {
+                StringBuffer buff = new StringBuffer();
+                while((i < expToken.length) && (Character.isDigit(expToken[i]) || expToken[i] == '.')) {
+                    buff.append(expToken[i++]);
                 }
                 i--;
-                operands.push(num);
-            } else if (c == '(') {
-                operations.push(c);   //push character to operators stack
+                numberStack.push(Double.parseDouble(buff.toString()));
             }
-            //Closed brace, evaluate the entire brace
-            else if (c == ')') {
-                while (operations.peek() != '(') {
-                    int output = performOperation(operands, operations);
-                    operands.push(output);   //push result back to stack
-                }
-                operations.pop();
+            else if(ch == '(') {
+                //push to the operator stack
+                operatorStack.push(ch);
             }
-
-            // current character is operator
-            else if (isOperator(c)) {
-                while (!operations.isEmpty() && precedence(c) <= precedence(operations.peek())) {
-                    int output = performOperation(operands, operations);
-                    operands.push(output);   //push result back to stack
+            else if(ch == ')') {
+                while(operatorStack.peek() != '(') {
+                    double output = performOperation(numberStack.pop(), numberStack.pop(), operatorStack.pop());
+                    numberStack.push(output);
                 }
-                operations.push(c);   //push the current operator to stack
+                operatorStack.pop();
+            }
+            else if(isOperator(ch)) {
+                while(!operatorStack.isEmpty() && (precedence(ch) < precedence(operatorStack.peek()))) {
+                    double output = performOperation(numberStack.pop(), numberStack.pop(), operatorStack.pop());
+                    numberStack.push(output);
+                }
+                operatorStack.push(ch);
             }
         }
 
-        while (!operations.isEmpty()) {
-            int output = performOperation(operands, operations);
-            operands.push(output);   //push final result back to stack
+        while(!operatorStack.isEmpty()) {
+            double output = performOperation(numberStack.pop(), numberStack.pop(), operatorStack.pop());
+            numberStack.push(output);
         }
-        return operands.pop();
+        return numberStack.peek();
     }
 
-    static int precedence ( char c)
-    {
-        switch (c) {
-            case '+':
-            case '-':
-                return 1;
-            case '*':
-            case '/':
-                return 2;
-            case '^':
-                return 3;
+    public int precedence(char op) {
+        if(op == '*' || op == '/') {
+            return 2;
+        }else if(op == '+' || op == '-') {
+            return 1;
+        }else if(op == '^') {
+            return 3;
         }
         return -1;
     }
 
-    public int performOperation (Stack < Integer > operands, Stack < Character > operations)
-    {
-        int a = operands.pop();
-        int b = operands.pop();
-        char operation = operations.pop();
-        switch (operation) {
-            case '+':
-                return a + b;
-            case '-':
-                return b - a;
-            case '*':
-                return a * b;
-            case '/':
-                if (a != 0)
-                    return b / a;
+    public boolean isOperator(char op) {
+        return (op == '+' || op == '-' || op == '*' || op == '/' || op == '^');
+    }
+
+    public double performOperation(Double a, Double b, Character op) {
+        switch(op) {
+            case '+' : return a+b;
+            case '-' : return b-a;
+            case '*' : return a*b;
+            case '^' : return Math.pow(b, a);
+            case '/' :
+                if(a == 0) {
+                    throw new UnsupportedOperationException("Cannot divide by 0");
+                }else {
+                    return b/a;
+                }
         }
         return 0;
     }
-
-    public boolean isOperator ( char c)
-    {
-        return (c == '+' || c == '-' || c == '/' || c == '*' || c == '^');
-    }
-
 }
